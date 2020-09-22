6353f5767d7ff705bc7b9d526213783aadcf787e
Dale Greve <dgreve@altair.com>
2020-09-22 11:23:41 -0400

    setup webpack

--- app.js
+++ app.js
@@ -3,5 +3,9 @@
 
 function sayHi() {
   console.log("HI")
-  console.log(this);
+  console.log(this); // refers to current execution scope, when in this function refers to Window
 }
+
+var color = "teal" // adds to Window in global scope
+let num = 400 // let and const do not add to global scope
+const myhat = 'fedora'
---
+++ webpack.prod.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
+    new HTMLWebpackPlugin({
+      title: 'Output Management',
+    })
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    './dist'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
+      }
+    ]
+  }
+}
