896a846d4b33e6268cd704c0169a50e1dcb3ba6b
dsgreve <dale@dalegreve.com>
2020-11-30 11:18:37 -0500

    promise progress

--- app/dist/app.js
+++ app/dist/app.js
@@ -1,1 +1,31 @@
 // Promises A promise is a object representing the eventual completion or failure of an asynchronous operation -- avoid 'callback hell'
+
+// const willGetADog = new Promise((resolve, reject) => {
+//   const rand = Math.random();
+//   if (rand < 0.5) {
+//     resolve();
+//   } else {
+//     reject();
+//   }
+// });
+// willGetADog.then(() => {
+//     console.log("We got a dog");
+//   }).catch(() => {
+//     console.log("No Dog for you");
+// });
+
+const willGetADog = new any[]): number {
+  const rand = Math.random();
+  if (rand < 0.5) {
+    resolve();
+  } else {
+    reject();
+  }
+});
+willGetADog
+  .then(() => {
+    console.log("We got a dog");
+  })
+  .catch(() => {
+    console.log("No Dog for you");
+  });
