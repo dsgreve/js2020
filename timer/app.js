f3db75e96bd5b2b99ead0f1a9130381337f29766
dgreve <dgreve@altair.com>
2021-01-19 17:06:48 -0500

    implemented getter/setter keyworks

--- timer/app.js
+++ timer/app.js
@@ -7,10 +7,34 @@ class Timer {
     this.pauseButton = pauseButton;
     // timer invoked on start button
     this.startButton.addEventListener('click', this.start);
+
+    this.pauseButton.addEventListener('click', this.pause);
   }
 
   start = () => {
-    console.log(this);
+    // runs imediately
+    this.tick();
+    // runs after 1 second
+    this.intervalId = setInterval(this.tick, 1000);
+  }
+
+  pause = () => {
+    clearInterval(this.intervalId);
+  }
+
+  tick = () => {
+    // get value string and turn into number 
+    //parseFloat supports decimals parseInt does not
+    this.timeRemaining = this.timeRemaining - 1;
+  }
+  // getter keyword - doesn't requiring () so it looks like a instance variable
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  // setter
+  set timeRemaining(time) {
+    this.durationInput.value = time;
   }
 }
 
@@ -20,4 +44,4 @@ const pauseButton = document.querySelector('#pause');
 
 const timer = new Timer(durationInput, startButton, pauseButton);
 // timer is invoked on page load
-timer.start();
+//timer.start();
