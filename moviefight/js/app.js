f15fe42cb296f6be075ac529b8f17c7cbd7513f4
dsgreve <dgreve@altair.com>
2021-06-02 21:24:52 -0400

    refactored autocomplete to be reusable in other projects

--- moviefight/js/app.js
+++ moviefight/js/app.js
@@ -1,73 +1,37 @@
-
 // omdb api http://www.omdbapi.com/?i=tt3896198&apikey=147b4d4f 
-const fetchData = async (searchTerm) => {
-  const response = await axios.get('//www.omdbapi.com/', {
-    params: {
-      apikey: '147b4d4f',
-      s: searchTerm
+
+createAutoComplete({
+  root: document.querySelector('.autocomplete'),
+  renderOption(movie) {
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+    return `
+      <img src="${imgSrc}" alt="${movie.Title}" />
+      ${movie.Title} (${movie.Year})
+    `;
+  },
+  onOptionSelect(movie) {
+    onMovieSelect(movie);
+  },
+  inputValue(movie) {
+    return movie.Title;
+  },
+
+  async fetchData(searchTerm) {
+    const response = await axios.get('//www.omdbapi.com/', {
+      params: {
+        apikey: '147b4d4f',
+        s: searchTerm
+      }
+    });
+
+    if (response.data.Error) {
+      return [];
     }
-  });
-  //console.log(response.data)
 
-  if (response.data.Error) {
-    return[];
+    return response.data.Search;
   }
-  return response.data.Search;
-};
-
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-  <label><b>Search For a Movie</b></label>
-  <input class="input" />
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results"></div>
-    </div>
-  </div>
-`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async event => {
-  const movies = await fetchData(event.target.value);
-
-  if (!movies.length) {
-    dropdown.classList.remove('is-active');
-    return;
-  }
-    resultsWrapper.innerHTML = ('');
-
-    dropdown.classList.add('is-active');
-
-    for (let movie of movies) {
-      const option = document.createElement('a');
-      
-      // ternary expression
-      const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-      option.classList.add('dropdown-item');
-      option.innerHTML = `
-        <img src="${imgSrc}" alt="${movie.Title}" />
-        ${movie.Title}
-      `;
-      option.addEventListener('click', () => {
-        dropdown.classList.remove('is-active');
-        input.value = movie.Title;
-        onMovieSelect(movie);
-      });
-
-      resultsWrapper.appendChild(option);
-    }
-};
-
-input.addEventListener('input', debounce(onInput, 500));
-
-document.addEventListener('click', event => {
- if (!root.contains(event.target)) {
-   dropdown.classList.remove('is-active');
- }
 });
+
 
 // create helper function 
 const onMovieSelect = async movie => {
