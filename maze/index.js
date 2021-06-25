6e9c40460ed007e60292d6056219286ccc5848ba
dsgreve <dgreve@altair.com>
2021-06-25 17:04:24 -0400

    added grid true false statements

--- maze/index.js
+++ maze/index.js
@@ -1,11 +1,12 @@
-const { 
-  Engine, 
-  Render, 
-  Runner, 
-  World, 
-  Bodies, 
+const {
+  Engine,
+  Render,
+  Runner,
+  World,
+  Bodies,
 } = Matter;
 
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -48,8 +49,82 @@ World.add(world, walls)
 //   }
 // }
 
-const grid = Array(3).fill(true).map(() => Array(3).fill(false));
+// maze generation
+
+const shuffle = (arr) => {
+  let counter = arr.length;
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+
+    counter--;
+
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+
+  return arr;
+};
+
+const grid = Array(cells)
+  .fill(true)
+  .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+  .fill(null)
+  .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
 
 
+const stepThroughCell = (row, column) => {
+  // If i have visited a cel, then return
+  if (grid[row][column]) {
+    return;
+  }
 
-console.log(grid);
+  // mark cell as visited
+  grid[row][column] = true;
+  // assemble radomly-ordered neighbor
+  const neighbors = shuffle([
+    [row - 1, column, 'up'],
+    [row, column + 1, 'right'],
+    [row + 1, column, 'down'],
+    [row, column - 1, 'left']
+  ]);
+
+  // For each neighbor
+  for (let neighbor of neighbors) {
+    const [nextRow, nextColumn, direction] = neighbor;
+
+    // see if neighbor is out of bounds
+    if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+      continue;
+    }
+
+    // If we visited that neighbor, continue to next neighbor
+    if (grid[nextRow][nextColumn]) {
+      continue;
+    }
+
+    // Remove a wall from either horizontals or verticals
+    if (direction === 'left') {
+      verticals[row][column - 1] = true;
+    } else if (direction === 'right') {
+      verticals[row][column] = true;
+    } else if (direction === 'up') {
+      horizontals[row - 1][column] = true;
+    } else if (direction === 'down') {
+      horizontals[row][column] = true;
+    }
+  }
+  // visit that next cell
+};
+console.log(verticals, horizontals);
+stepThroughCell(startRow, startColumn);
