12612c35b5662bff8556369b5cca44c7c2b5d30f
dsgreve <dgreve@altair.com>
2022-10-19 16:18:17 -0400

    scroll intersect

---
+++ wes-bos/excercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -0,0 +1,26 @@
+function scrollToAccept() {
+  const terms = document.querySelector('.terms-and-conditions');
+  const watch = document.querySelector('.watch');
+  const button = document.querySelector('.accept');
+  if (!terms) {
+    return; // quit this there aren't terms on the page
+  }
+  
+  // intersection observer
+  function obCallback(payload) {
+    if (payload[0].intersectionRatio === 1) {
+      button.disabled = false;
+    } else {
+      button.disabled = true;
+    }
+  }
+  const ob = new IntersectionObserver(obCallback, {
+    root: terms,
+    threshold: 1,
+  });
+
+  ob.observe(terms.lastElementChild);
+
+}
+
+scrollToAccept();
