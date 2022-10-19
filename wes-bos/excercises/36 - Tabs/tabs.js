6e355047e8da20489acdb971980421dba8e9409d
dsgreve <dale@dalegreve.com>
2022-10-18 20:43:52 -0400

    finished tabs

--- wes-bos/excercises/36 - Tabs/tabs.js
+++ wes-bos/excercises/36 - Tabs/tabs.js
@@ -1,1 +1,35 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
+function handleTabClick(e) {
+  // hide all tab panels
+  // tabPanels.forEach(function(panel) {
+  //   panel.hidden = true;
+  // })
+  tabPanels.forEach(panel => {
+    panel.hidden = true;
+  });
+  // mark all tabs as unselected
+  tabButtons.forEach(tab => {
+    // anytime you see a dash on an html element you can access via camelCase
+    //tab.ariaSelected = false;
+    tab.setAttribute('aria-selected', false);
+
+  });
+  // mark clicked tab as selected
+  e.currentTarget.setAttribute('aria-selected', true);
+  // find associated tab panel and show
+  const {id} = e.currentTarget;
+  /* METHOD 1
+  const tabPanel = tabs.querySelector(`[aria-labeledby=${id}]`);
+  console.log(tabPanel);
+  tabPanel.hidden = false;
+  */
+  
+  // Method 2: Find in the array of tab panels
+  const tabPanel = tabPanels.find(panel => panel.getAttribute('aria-labeledby') === id);
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick))
